c079f93dc361635c8a515e057d25c1a9a91ad159
AnjalinRose <130470551+AnjalinRose@users.noreply.github.com>
2023-04-12 18:20:58 +0530

    Add files via upload

---
+++ python/forloop.py
@@ -0,0 +1,28 @@
+#for i in range(1,10,2):
+  #  print(i)
+#a=["mango", "orange","apple"]
+#for i in a:
+ #   print(i)
+#name = 'python'
+# print(i) 
+#colour =["red","green","blue" ]
+#fruits=["mango","apple",]
+#for x in colour:
+ # for y in fruits:
+   #     print(x,y) 
+#for i in range(3):
+   # for j in range(3):
+    #    print(i,j) 
+list=[]
+for i in range(1,101):
+    list.append(i)
+print(list)
+list2=[]
+list3=[]
+for i in list:
+    if i%2==0:
+        list2.append(i)
+    else:
+        list3.append(i)
+print(list2)
+print(list3)
---
+++ python/sample.py
@@ -0,0 +1,15 @@
+x=int(input("enter a number"))
+i=1
+while i<=x:
+    
+    
+    if(i==8):
+        i=i+1
+        
+        continue
+   
+   
+    
+    print(i)
+    i=i+1
+    
